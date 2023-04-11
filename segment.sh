c593e5c0eeb3fdc158ac2705a2fd5aaa645e94ac
bacaron <bacaron245@gmail.com>
2023-04-10 21:51:57 -0500

    reason: updates for new app

--- segment.sh
+++ segment.sh
@@ -2,26 +2,16 @@
 
 # top variables
 track=`jq -r '.track' config.json`
-indices=`jq -r '.selectIndices' config.json`
 parc=`jq -r '.parcellations' config.json`
 
 [ ! -f parc.nii.gz ] && cp ${parc} ./parc.nii.gz
 
 # generate individual tracts for each index
-connectome2tck ${track} assignments.txt track -files per_node
+tck2connectome ${track} parc.nii.gz -out_assignments track_assignments.txt tmp.csv -force
+connectome2tck ${track} assignments_both_endpoints.txt track -file per_node
 
-# if user doesn't input selected tract indices, make indices variable to all indices found in assignments.txt
-if [ -z ${indices} ]; then
+if [ ! -f track/track.tck ]; then
 	holder=(*track*.tck)
-	indices=""
-	for (( i=0; i<${#holder[*]}; i++ ));
-	do
-		indices="$indices $((i+1))"
-	done
+	tckedit ${holder[*]} track/track.tck
+	tckinfo ./track/track.tck >> track/track_info.txt
 fi
-
-# loop through all indices to combine
-for i in ${indices}
-do
-	tck2connectome track${i}.tck parc.nii.gz -out_assignments track${i}_assignments.txt tmp.csv -force
-done
