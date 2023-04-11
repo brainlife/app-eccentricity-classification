f534bc4ccf494bd98a8b1441c0c905b06ab85980
Brad Caron <bacaron@utexas.edu>
2023-04-11 15:06:51 -0500

    reason: updated commits

--- ecc_segment.sh
+++ ecc_segment.sh
@@ -2,31 +2,8 @@
 
 # top variables
 track=`jq -r '.track' config.json`
-# indices=`jq -r '.selectIndices' config.json`
 
-# if user doesn't input selected tract indices, make indices variable to all indices found in assignments.txt
-# if [ -z ${indices} ]; then
-# 	holder=(*track*.tck)
-# 	indices=""
-# 	for (( i=0; i<${#holder[*]}; i++ ));
-# 	do
-# 		indices="$indices $((i+1))"
-# 	done
-# fi
-
-# loop through all indices to segment by eccentricity
-# for i in ${indices}
-# do
 connectome2tck ${track} assignments_both_endpoints.txt track -file per_node
-# done
-
-[ ! -d tmp ] && mkdir tmp 
-mv track*_parc*.tck ./tmp/
-mv names.txt ./tmp/
-
-mv *.tck *.txt *.nii.gz ./raw/
-
-mv ./tmp/* ./ && rm -rf tmp
 
 # create new tractogram
 if [ ! -f track/track.tck ]; then
--- segment.sh
+++ segment.sh
@@ -3,15 +3,18 @@
 # top variables
 track=`jq -r '.track' config.json`
 parc=`jq -r '.parcellations' config.json`
+both_endpoints=`jq -r '.both_endpoints' config.json`
 
 [ ! -f parc.nii.gz ] && cp ${parc} ./parc.nii.gz
 
 # generate individual tracts for each index
 tck2connectome ${track} parc.nii.gz -out_assignments track_assignments.txt tmp.csv -force
-connectome2tck ${track}  track_assignments.txt track -file per_node
 
-if [ ! -f track/track.tck ]; then
-	holder=(*track*.tck)
-	tckedit ${holder[*]} track/track.tck
-	tckinfo ./track/track.tck >> track/track_info.txt
+if [[ ${both_endpoints} != true ]]; then
+	connectome2tck ${track}  track_assignments.txt track -file per_node
+	if [ ! -f track/track.tck ]; then
+		holder=(*track*.tck)
+		tckedit ${holder[*]} track/track.tck
+		tckinfo ./track/track.tck >> track/track_info.txt
+	fi
 fi
